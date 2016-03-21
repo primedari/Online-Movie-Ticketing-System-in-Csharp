b47530ad9b51c9a7ea8654f64483553157180d04
primedari <priyanka.medari@gmail.com>
2016-03-20 18:12:37 -0700

    Added files via upload
    Interface IBooking

---
+++ IBooking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTime.Business
+{
+
+    interface IBooking      // interface
+    {
+        double CalculatePrice(); // method to calculateprice
+    }
+}
