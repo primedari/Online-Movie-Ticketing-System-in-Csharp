62a33a89a50d2927610534eeb6e49ab5ef0e5ae4
primedari <priyanka.medari@gmail.com>
2016-03-20 18:06:52 -0700

    Added files via upload
    BookingList Class

---
+++ BookingList.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MovieTime.Business
+{
+
+    //Generic Class BookingList implements IEnumerable interface
+    class BookingList<T> : IEnumerable
+    {
+        public List<T> ListBook { get; set; }
+        public BookingList()
+        {
+            ListBook = new List<T>();
+        }
+        public void Add(T obj)
+        {
+            ListBook.Add(obj); // adds the object to arraylist
+        }
+        public IEnumerator GetEnumerator()
+        {
+            foreach (T someBook in ListBook)  // itreate through arraylist 
+                yield return someBook;
+        }
+    }
+}
